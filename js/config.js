4ebc54ba0df7f4bb183facb928c7c1ce40f2ea5e
baback <baback@users.noreply.github.com>
2023-02-11 12:42:52 -0800

    test

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
-// $(function(){
-//   $("body").append('<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>');
-//   $("body").append('<script src="js/jquery-contenteditable.min.js"></script>');
-//   $("body").append('<script src="https://kit.fontawesome.com/2ac67565a3.js" crossorigin="anonymous"></script>');
-//   $("body").append('<script src="js/jquery.sidebar.min.js"></script>');
-//   $("body").append('<script src="js/editor.js"></script>  ');
-// });
+  $("body").append('<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>');
+  $("body").append('<script src="js/jquery-contenteditable.min.js"></script>');
+  $("body").append('<script src="https://kit.fontawesome.com/2ac67565a3.js" crossorigin="anonymous"></script>');
+  $("body").append('<script src="js/jquery.sidebar.min.js"></script>');
+  $("body").append('<script src="js/editor.js"></script>  ');
 $.getJSON("/js/config.json", function(data) {
   var tier = data.tier;
   if (tier === "premium") {
